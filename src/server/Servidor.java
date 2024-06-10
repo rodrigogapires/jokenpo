a3fc22f1741eca7e733f4d5b5006f7dc739c78c0
Rodrigo Pires <pires.rodrigo@outlook.com.br>
2024-06-10 16:36:43 -0300

    add src/server/Servidor.java

---
+++ src/server/Servidor.java
@@ -0,0 +1,30 @@
+package server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Servidor {
+    protected static ArrayList<Atende> atendentes = new ArrayList<>();
+
+    public static void main(String[] args) {
+        final int PORTA = 9876;
+        ServerSocket serverSocket;
+        Socket cliente;
+
+        try {
+            serverSocket = new ServerSocket(PORTA);
+
+            while (true) {
+                System.out.println("Aguardando o cliente...");
+                cliente = serverSocket.accept();
+                Atende atende = new Atende(cliente);
+                atendentes.add(atende);
+                atende.start();
+            }
+
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+}
